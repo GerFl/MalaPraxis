5485949c4aeb3e9c1550b1db90118da2bb68b8c1
GerFl <flores20_03@outlook.com>
2019-09-26 14:22:02 -0500

    Agregué Simplex

---
+++ Simplex.cpp
@@ -0,0 +1,23 @@
+#include<stdio.h>
+#include<conio.h>
+#include<stdlib.h>
+
+main(){
+	int i,j,v,m,n,r;
+	int M[m][n];
+	int maxmin;
+	printf("¿Desea maximizar o minimizar?\n1.Maximizar\t2.Minimizar\t");
+	scanf("%d",&maxmin);
+	printf("Ingrese la cantidad de variables en el sistema: %d\n",&maxmin);
+	scanf("%d",&v);
+	/*CORREGIR PROBLEMAS DE DIRECCIÓN	
+	for(m=0;m<v;m++){
+		for(n=0;n<v;n++){
+		printf("Ingrese el coeficiente de la variable %d en la ecuación %d ",&n,&m);
+		scanf("%d",&j);
+		}
+	printf("Ingrese el resultado de la ecuación: ");
+	scanf("%d",&r);	
+	}*/
+	getche();
+}
