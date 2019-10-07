71e6dff871b8c49cbba18071f90a346353e91da0
GerFl <flores20_03@outlook.com>
2019-10-07 17:03:39 -0500

    Modificar almacenamiento de coeficientes

--- Simplex.cpp
+++ Simplex.cpp
@@ -8,29 +8,35 @@ main(){
 	// maxmin = maximización o minimización
 	// vr = valor de la restricción
 	// v = # de varibles en el sistema
-	int c,m,n,v,r,vr;
+	int m,n;
 	int M[m][n];
-	int maxmin;
 	int varholg;
-	/* MAXIMIZAR O MINIMIZAR
-	while(maxmin<0 || maxmin>2){
-	printf("¿Desea maximizar o minimizar?\n1.Maximizar\t2.Minimizar\t");
-	scanf("%d",&maxmin);
-	if(maxmin<0||maxmin>2){
-		printf("\nFavor de indicar el número de la opción mostrada.\n");
-	}
-	}*/
+	void restricciones();
+	void max_min();
+	max_min();
+	restricciones();
+	getche();
+}
+
+void max_min(){
+	int maxmin;
+	do{
+		printf("%cDesea maximizar o minimizar?\n1.Maximizar\t2.Minimizar\t",168);
+		scanf("%d",&maxmin);
+	} while(maxmin<1 || maxmin>2);
+}
+void restricciones(){
+	int m,n,c,v,r,vr;
 	printf("Ingrese la cantidad de restricciones en el sistema: ");
 	scanf("%d",&r);
 	printf("Ingrese la cantidad de variables: ");
 	scanf("%d",&v);
 	for(m=0;m<r;m++){
 		for(n=0;n<v;n++){
-		printf("Ingrese el coeficiente de la variable %d en la ecuaci%cn %d ",&n,162,&m);
-		scanf("%d",&c);
+			printf("Ingrese el coeficiente de la variable %d en la ecuaci%cn %d ",&n,162,&m);
+			scanf("%d",&c);
 		}
 	printf("Ingrese el resultado de la ecuaci%cn: ",162);
 	scanf("%d",&vr);	
 	}
-	getche();
 }
