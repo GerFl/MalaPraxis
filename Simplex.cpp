c4b55d00a0fbc5d4f3d478b09ff6fbdf0243ce9e
GerFl <flores20_03@outlook.com>
2019-10-15 15:24:50 -0500

    Validar string

--- Simplex.cpp
+++ Simplex.cpp
@@ -1,19 +1,21 @@
 #include<stdio.h>
 #include<conio.h>
 #include<stdlib.h>
+#include<ctype.h>
 main(){
 	// r = # de restricciones
 	// varholg = variables de holgura. El número de variables de holgura es igual a la cantidad de res
 	// maxmin = maximización o minimización
 	// vr = valor de la restricción
 	// v = # de varibles en el sistema
+	// validación de string
 	int m,n;
 	int M[m][n];
 	int varholg;
-	void restricciones();
+	void captura_restricciones();
 	void max_min();
 	max_min();
-	restricciones();
+	captura_restricciones();
 	getche();
 }
 
@@ -22,21 +24,27 @@ void max_min(){
 	do{
 		printf("%cDesea maximizar o minimizar?\n1.Maximizar\t2.Minimizar\t",168);
 		scanf("%d",&maxmin);
+		char str[0];
+		if(isdigit(maxmin[""])){
+			printf("Khe");
+		}
 	} while(maxmin<1 || maxmin>2);
 }
 
-void restricciones(){
-	int m,n,c,v,r,vr;
+void captura_restricciones(){
+	int m,n,c,v,r,vr,e;
 	int R[50][50];
 	int V[50][50];
 	printf("Ingrese la cantidad de restricciones en el sistema: ");
 	scanf("%d",&r);
 	printf("Ingrese la cantidad de variables: ");
 	scanf("%d",&v);
-	for(m=0;m<r;m++){
-		for(n=0;n<v;n++){
-			printf("Ingrese el coeficiente de la variable %d en la ecuaci%cn %d ",&n,162,&m);
-			scanf("%d",&n);
+	for(m=1;m<=r;m++){
+		for(n=1;n<=v;n++){
+			printf("Ingrese el coeficiente de la variable %i en la restricci%cn %i: ",n,162,m);
+			scanf("%d",&e);
+			//V[r][v]=n;
+			//printf("%d\n",V[r][v]);
 		}
 		printf("Ingrese el resultado de la ecuaci%cn: ",162);
 		scanf("%d",&vr);	
