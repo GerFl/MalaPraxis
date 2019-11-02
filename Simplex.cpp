15b089cb933dcaadfe0746eb33c5b5fad249b81c
GerFl <flores20_03@outlook.com>
2019-11-01 18:01:02 -0600

    asdf movie 1

--- Simplex.cpp
+++ Simplex.cpp
@@ -3,19 +3,24 @@
 #include<stdlib.h>
 #include<ctype.h>
 main(){
+	// checar especificaciones de la función objetivo
 	// r = # de restricciones
 	// varholg = variables de holgura. El número de variables de holgura es igual a la cantidad de res
 	// maxmin = maximización o minimización
 	// vr = valor de la restricción
-	// v = # de varibles en el sistema
+	// v = # de variables en el sistema
 	// validación de string
+	// función de tablatura
+	// resolver función objetivo
 	int m,n;
 	int M[m][n];
 	int varholg;
 	void captura_restricciones();
 	void max_min();
+	void tabulacion();
 	max_min();
 	captura_restricciones();
+	//tabulacion();
 	getche();
 }
 
@@ -32,9 +37,16 @@ void max_min(){
 }
 
 void captura_restricciones(){
-	int m,n,c,v,r,vr,e;
-	int R[50][50];
+	int m,n,v,r,vr,e;
+	int f,fo,i,piv;
+	int R[50];
 	int V[50][50];
+	int F[50];
+	/*printf("Ingrese la función objetivo: ");
+	for(f=0;fo<?;f++){
+		scanf("%d",&fo);
+		F[f]=fo;
+	}*/
 	printf("Ingrese la cantidad de restricciones en el sistema: ");
 	scanf("%d",&r);
 	printf("Ingrese la cantidad de variables: ");
@@ -43,10 +55,28 @@ void captura_restricciones(){
 		for(n=1;n<=v;n++){
 			printf("Ingrese el coeficiente de la variable %i en la restricci%cn %i: ",n,162,m);
 			scanf("%d",&e);
-			//V[r][v]=n;
-			//printf("%d\n",V[r][v]);
+			V[m][n]=e;			
 		}
 		printf("Ingrese el resultado de la ecuaci%cn: ",162);
-		scanf("%d",&vr);	
+		scanf("%d",&vr);
+		R[m]=vr;	
+	}
+	for(m=1;m<=r;m++){
+		printf("\nArreglo %d: ",m);
+		for(n=1;n<=v;n++){
+			printf("%d, ",V[m][n]);			
+		}
+		printf("Resultado de la ecuaci%cn: %d",162, R[m]);
+		//printf("%d",R[m]);
 	}
 }
+
+/*void tabulacion(){
+	int i,j,r,v;
+	int V[r][v];
+	for(i=0;i<=r;i++){
+		for(j=0;j<=v;j++){
+			printf("%d",&V[r][v]);
+		}
+	}
+}*/
